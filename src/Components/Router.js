f4c64ee3096fc6c4bdcbcbb0001c887febf36204
hanji <abhidhamma91@gmail.com>
2019-12-11 22:39:03 +0900

    g

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Auth from '../Routes/Auth';
 import Menu from '../Components/Menu';
 import People from '../Routes/People';
@@ -67,9 +67,9 @@ const LoggedOutRoutes = () => (
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
